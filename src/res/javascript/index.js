5a21f786303ef34c581270d7c199d3e4d639c368
Hans-Christian Knolle <hanscknolle08@gmail.com>
2025-02-10 15:36:20 +0100

    Update 1.0.19 Started work on Dictionary and released first Pre-Release of it.

--- src/res/javascript/index.js
+++ src/res/javascript/index.js
@@ -17,3 +17,27 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     window.addEventListener("scroll", highlightSection);
 });
+
+function searchTerm() {
+    const searchValue = document.getElementById('searchField').value.toLowerCase();
+    const sections = document.querySelectorAll('section');
+
+    for (let section of sections) {
+        const sectionTitleElement = section.querySelector('h3');
+        
+        if (sectionTitleElement) {
+            const sectionTitle = sectionTitleElement.innerText.toLowerCase();
+
+            if (sectionTitle.includes(searchValue)) {
+                section.scrollIntoView({ behavior: 'smooth' });
+                
+                sectionTitleElement.classList.add('highlight');
+
+                setTimeout(() => {
+                    sectionTitleElement.classList.remove('highlight');
+                }, 1000);
+                break;
+            }
+        }
+    }
+}
