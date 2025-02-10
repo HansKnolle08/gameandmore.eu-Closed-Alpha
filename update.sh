6fcedfdb403b6b7e33a830909a8578ca7c158926
Hans-Christian Knolle <hanscknolle08@gmail.com>
2025-02-10 13:53:58 +0100

    Added check for updates into update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Setze den temporären Pfad und das Zielverzeichnis
+# Setze die Pfade
 temp_dir="/home/temp"
-target_dir="/var/www/html"
 repo_url="https://github.com/HansKnolle08/gameandmore.eu.git"
+web_dir="/var/www/html"
+update_dir="/var/www/temp"
 
 # Alte Daten aus temp entfernen
 rm -rf "$temp_dir/gameandmore.eu"
@@ -12,21 +13,26 @@ rm -rf "$temp_dir/gameandmore.eu"
 mkdir -p "$temp_dir"
 git clone "$repo_url" "$temp_dir/gameandmore.eu"
 
-# Altes Webverzeichnis entfernen und neu erstellen
-rm -rf "$target_dir"
-mkdir -p "$target_dir"
+# Update-Verzeichnis vorbereiten
+rm -rf "$update_dir"
+mkdir -p "$update_dir"
 
-# Dateien und Ordner aus src verschieben
-mv "$temp_dir/gameandmore.eu/LICENSE" "$target_dir/"
-mv "$temp_dir/gameandmore.eu/src/index.html" "$target_dir/"
-mv "$temp_dir/gameandmore.eu/src/res" "$target_dir/"
+# Dateien und Ordner aus src in das Update-Verzeichnis verschieben
+mv "$temp_dir/gameandmore.eu/LICENSE" "$update_dir/"
+mv "$temp_dir/gameandmore.eu/src/index.html" "$update_dir/"
+mv "$temp_dir/gameandmore.eu/src/res" "$update_dir/"
 
-# Berechtigungen setzen
-chown -R www-data:www-data "$target_dir"
-chmod -R 755 "$target_dir"
-
-# Skript selbst nach / kopieren
-cp "$0" /
+# Prüfen, ob sich die Dateien geändert haben
+if diff -qr "$web_dir" "$update_dir" > /dev/null; then
+    echo "Keine Änderungen gefunden. Bereinige Update-Verzeichnis."
+    rm -rf "$update_dir"
+else
+    echo "Änderungen erkannt. Aktualisiere Webseite."
+    rm -rf "$web_dir"
+    mv "$update_dir" "$web_dir"
+    chown -R www-data:www-data "$web_dir"
+    chmod -R 755 "$web_dir"
+fi
 
 # Cleanup
-echo "Deployment abgeschlossen."
+echo "Update abgeschlossen."
